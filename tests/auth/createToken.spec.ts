7414dcd52a29a261b461cef28640f09f11bef56b
Gabriel Cayoja <gabriel271085@gmail.com>
2026-06-08 16:54:50 -0400

    fix createToken response variable

--- tests/auth/createToken.spec.ts
+++ tests/auth/createToken.spec.ts
@@ -5,14 +5,19 @@ test('POST auth should return a token', async ({request}) =>{
 
     const authClient = new AuthClient();
 
-    const token = await authClient.getToken(request);
+    const authPayload = {
+        username: process.env.AUTH_USERNAME,
+        password: process.env.AUTH_PASSWORD,
+    
+    };
+
+    const response = await authClient.createToken(request, authPayload);
 
    
     expect(response.status()).toBe(200)
 
     const body = await response.json();
 
-    console.log(body);
     expect(body.token).toBeDefined();
     expect(typeof body.token).toBe('string');
     expect(body.token.length).toBeGreaterThan(0);
